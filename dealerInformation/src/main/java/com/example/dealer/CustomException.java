bf52f81ce842c1e58dbad19fd1825a19470cb6fd
onkar <onkar@gmail.com>
2020-08-25 13:44:00 +0530

    added logging, status code, exception handling functionlity

---
+++ dealerInformation/src/main/java/com/example/dealer/CustomException.java
@@ -0,0 +1,9 @@
+package com.example.dealer;
+
+
+	public class CustomException extends Exception {
+	    public CustomException() {
+	        super("Error While API Hit");
+	    }
+	}
+
--- dealerInformation/src/main/java/com/example/dealer/DealerDetailsController.java
+++ dealerInformation/src/main/java/com/example/dealer/DealerDetailsController.java
@@ -1,8 +1,14 @@
 package com.example.dealer;
 
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,36 +20,75 @@ import org.springframework.web.bind.annotation.RestController;
 public class DealerDetailsController {
     @Autowired
     private DealerDetailsServiceImpl  dealerDetailsServiceImpl;
-  
+    Logger logger = LoggerFactory.getLogger(DealerDetailsController.class);
+    
     @GetMapping("/dealer") 
     public Iterable<DealerDetails> findAll() {
        Iterable<DealerDetails> dealers = dealerDetailsServiceImpl.findAll();
        return dealers;
     }
 
-    @PostMapping("/dealer") 
-    public DealerDetails saveDealer(@RequestBody DealerDetails dealer) {
+    @SuppressWarnings("rawtypes")
+	@PostMapping("/dealer") 
+    public ResponseEntity saveDealer(@RequestBody DealerDetails dealer) throws CustomException {
     	ModelMapper modelMapper = new ModelMapper();
-    	DealerDetails dealerResponse = modelMapper.map(dealerDetailsServiceImpl.saveDealer(dealer), DealerDetails.class);
-       return dealerResponse;
+          try {
+       	   DealerDetails dealerResponse = modelMapper.map(dealerDetailsServiceImpl.saveDealer(dealer), DealerDetails.class);
+       	  System.out.println(dealerResponse);
+       	   return new ResponseEntity<>( HttpStatus.CREATED) ;
+       	   
+          }
+          catch(Exception e) {
+           logger.error("Internal Server Error");
+       	   throw new CustomException();
+          }
+
     }
     
     @GetMapping("/dealer/{id}") 
-    public DealerDetails findByID(@PathVariable("id") Long id) {
+    public ResponseEntity<DealerDetails> findByID(@PathVariable("id") Long id)  throws CustomException{
        ModelMapper modelMapper = new ModelMapper();
-       DealerDetails dealerResponse = modelMapper.map(dealerDetailsServiceImpl.findByID(id), DealerDetails.class);
-       return dealerResponse;
+       try {
+    	   DealerDetails dealerResponse = modelMapper.map(dealerDetailsServiceImpl.findByID(id), DealerDetails.class);
+    	  
+    	   return new ResponseEntity<>(dealerResponse, HttpStatus.FOUND) ;
+    	   
+       }
+       catch(Exception e) {
+    	   logger.error("ID not exist");
+    	   throw new CustomException();
+       }
+
+       
     }
     
-    @PutMapping("/dealer") 
-    public DealerDetails updateDealer(@RequestBody DealerDetails dealer) {
-       ModelMapper modelMapper = new ModelMapper();
-       DealerDetails dealerResponse = modelMapper.map(dealerDetailsServiceImpl.updateDealer(dealer), DealerDetails.class);
-       return dealerResponse;
+    @SuppressWarnings("rawtypes")
+	@PutMapping("/dealer") 
+    public ResponseEntity updateDealer(@RequestBody DealerDetails dealer) throws CustomException {
+    	ModelMapper modelMapper = new ModelMapper();
+        try {
+     	   DealerDetails dealerResponse = modelMapper.map(dealerDetailsServiceImpl.updateDealer(dealer), DealerDetails.class);
+     	  System.out.println(dealerResponse);
+     	   return new ResponseEntity<>( HttpStatus.CREATED) ;
+     	   
+        }
+        catch(Exception e) {
+        	logger.error("Record not created");
+     	   throw new CustomException();
+        }
+
     }
     
     @DeleteMapping("/dealer/{id}") 
-    public void deleteByID(@PathVariable("id") Long id) {
-       dealerDetailsServiceImpl.deleteByID(id);
+    public void deleteByID(@PathVariable("id") Long id) throws CustomException {
+    	try {
+    		 dealerDetailsServiceImpl.deleteByID(id);
+    	}
+      catch(Exception e) {
+    	  logger.error("Record not found");
+    	  throw new CustomException();
+      }
     }
+    
+
 }
